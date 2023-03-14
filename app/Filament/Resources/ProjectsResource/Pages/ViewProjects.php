c082738a6a79c9c34d00d6441b574beb0e690204
Marjose <hypernate1@gmail.com>
2023-03-14 14:54:18 +0800

    added mutation

--- app/Filament/Resources/ProjectsResource/Pages/ViewProjects.php
+++ app/Filament/Resources/ProjectsResource/Pages/ViewProjects.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\ProjectsResource\Pages;
 
 use App\Filament\Resources\ProjectsResource;
+use App\Models\User;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\ViewRecord;
 
@@ -14,6 +15,21 @@ class ViewProjects extends ViewRecord
     {
         return [
             Actions\EditAction::make(),
+            Actions\DeleteAction::make(),
+            Actions\ForceDeleteAction::make()
+                ->visible(fn(): bool => isset($this->record->deleted_at) ?? false),
+            Actions\RestoreAction::make()
+                ->visible(fn(): bool => isset($this->record->deleted_at) ?? false),
         ];
     }
+
+    protected function mutateFormDataBeforeFill(array $data): array
+    {
+        $data['created_by'] = User::find($data['created_by'])->creator;
+        $data['updated_by'] = User::find($data['updated_by'])->editor;
+        $data['deleted_by'] = User::find($data['deleted_by'])->destroyer;
+
+        return $data;
+    }
+
 }
