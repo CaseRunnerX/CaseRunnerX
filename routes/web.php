40aa9f886a465a20319a8c21be4c485aab0436e4
Marjose <hypernate1@gmail.com>
2023-03-14 12:02:19 +0800

    redirect route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+
+Route::redirect('/', '/'.config('filament.path'));
