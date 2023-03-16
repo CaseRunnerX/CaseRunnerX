973de1f05b15b7c59966bd23e1972741108ac8af
Marjose <hypernate1@gmail.com>
2023-03-16 17:12:02 +0800

    added fillables

--- app/Models/Run.php
+++ app/Models/Run.php
@@ -9,4 +9,24 @@ use Wildside\Userstamps\Userstamps;
 class Run extends Model
 {
     use SoftDeletes, Userstamps;
+
+    protected $fillable = [
+        'test_run_date',
+        'test_run_name',
+        'test_run_name',
+        'milestone_id',
+        'description',
+        'assigned_qa',
+        'test_suite_id',
+        'status',
+        'status',
+        'defect',
+        'failure',
+        'effect',
+        'root_cause',
+        'issue_id',
+        'created_by',
+        'updated_by',
+        'deleted_by'
+    ];
 }
