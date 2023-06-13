15bb62107ea30a00d63287aeaacb6ab87699b903
Marjose <hypernate1@gmail.com>
2023-06-13 12:56:57 +0800

    change to config

---
+++ config/tcms.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    /*
+     *  Application Name lookup in Project Creation
+     */
+
+    'applications' => [
+        'Zoho' => 'Zoho',
+        'SugarCRM' => 'Sugar CRM',
+        'SuiteCRM' => 'Suite CRM',
+        'Laravel' => 'Laravel'
+    ],
+
+    /*
+     *  Project Type lookup in the Project Creation
+     */
+    'project_type' => [
+        'Fix Bid' => 'Fix Bid',
+        'Premium Support' => 'Premium Support',
+    ],
+
+
+
+];
