fab75484ad43baa5010208ab740668c38cdb8170
Marjose <hypernate1@gmail.com>
2023-06-13 17:28:13 +0800

    extend Backups.php

---
+++ app/Filament/Pages/Backups.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use BezhanSalleh\FilamentShield\Traits\HasPageShield;
+use ShuvroRoy\FilamentSpatieLaravelBackup\Pages\Backups as BaseBackups;
+
+class Backups  extends BaseBackups
+{
+    use HasPageShield;
+
+}
